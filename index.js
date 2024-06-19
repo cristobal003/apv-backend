a90a43b2ae21dcd5ed180c3da28584ab79f20def
cristobal <gonzalezpallarescristobal@gmail.com>
2024-06-19 17:12:11 +0200

    fix

--- index.js
+++ index.js
@@ -15,18 +15,11 @@ conectarDB();
 
 const dominiosPermitidos = [process.env.FRONTEND_URL];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (dominiosPermitidos.indexOf(origin) !== -1) {
-      //El origen del Request esta permitido
-      callback(null, true);
-    } else {
-        callback(new Error('No permitido por CORS'));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
 
 
 app.use("/api/veterinarios", veterinarioRoutes);
